46c1b2ca828176251620a08360c3e5092878eca9
Carlos Andres Alzate <un.artbliss@gmail.com>
2016-03-03 13:31:50 -0300

    Cambios a la pacicion de los colores

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 /*Variables*/
 var colores = [
-	["#2ecc71", "#27ae60"],
-  	 ["#1abc9c", "#16a085"],
-  	 ["#e74c3c", "#c0392b"],
-  	 ["#9b59b6", "#8e44ad"],
-  	 ["#66cc66", "#17ad49"],
-  	 ["#2A7E64", "#267359"],
-  	 ["#4F82C4", "#6289CB"]
+     ["#2ecc71", "#27ae60"],
+       ["#1abc9c", "#16a085"],
+       ["#e74c3c", "#c0392b"],
+       ["#9b59b6", "#8e44ad"],
+       ["#66cc66", "#17ad49"],
+       ["#2A7E64", "#267359"],
+       ["#4F82C4", "#6289CB"]
 ]
 
 /*Funciones*/
 
 function randomNum(e) {
-	return Math.floor(((Math.random() * e.length) +1) - 1);
+    return Math.floor(((Math.random() * e.length) + 1) - 1);
 }
 
 function randomBackgroundColor() {
-	var ranNum = randomNum(colores);
-	return colores[ranNum];
+    var ranNum = randomNum(colores);
+    return colores[ranNum];
 }
 
-function randomTituloColor () {
-	var ranNum = randomNum(colores);
-	return colores[ranNum];
+function randomTituloColor() {
+    var ranNum = randomNum(colores);
+    return colores[ranNum];
 }
 
 /*variables*/
@@ -37,16 +37,16 @@ var text = document.getElementById('text');
 /*funciones*/
 
 azul.onclick = function () {
-	var ranColor = randomBackgroundColor();
-	bodyColor.style.backgroundColor = ranColor[0];
+    var ranColor = randomBackgroundColor();
+    bodyColor.style.backgroundColor = ranColor[0];
 }
 
 rosa.onclick = function () {
-	var ranColor =	randomBackgroundColor();
-	botones.style.backgroundColor = ranColor[0];
+    var ranColor = randomBackgroundColor();
+    botones.style.backgroundColor = ranColor[0];
 }
 
 rojo.onclick = function () {
-	var ranText = randomTituloColor();
-	text.style.color = ranText[0];	
+    var ranText = randomTituloColor();
+    text.style.color = ranText[1];
 }
